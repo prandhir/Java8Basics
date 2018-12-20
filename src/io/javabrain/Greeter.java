0907ce9575c8ac01908d253aa237d2e6bd945eda
Pankaj <pankaj.randhir@gmail.com>
2018-12-20 16:03:27 -0500

    extended greet method

--- src/io/javabrain/Greeter.java
+++ src/io/javabrain/Greeter.java
@@ -2,13 +2,28 @@ package io.javabrain;
 
 public class Greeter {
 
-    public void greeter(Greeting greeting){
+    public void greetNow(Greeting greeting){
         greeting.greet();
     }
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        SpanishGreeting spanishGreeting= new SpanishGreeting();
-        greeter.greeter(spanishGreeting);
+        //normal implementation
+        Greeting spanishGreeting= new SpanishGreeting();
+
+        //Lambda implementation
+        Greeting myLambdaGreeting = () -> System.out.println("Hello world from Lambda");
+
+        //inner class implementation
+        Greeting innerClassGreeting = new Greeting(){
+            public void greet(){
+                System.out.println("Hello world from inner class");
+            };
+        };
+
+        greeter.greetNow(myLambdaGreeting);
+        myLambdaGreeting.greet();
+        innerClassGreeting.greet();
+
     }
 }
---
+++ src/io/javabrain/RunnableExample.java
@@ -0,0 +1,20 @@
+package io.javabrain;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running from thread 1");
+            }
+        });
+
+        myThread.run();
+
+        Runnable myrunnableLambda = () -> System.out.println("This is from labda thread.");
+
+        Thread myThread2 = new Thread(myrunnableLambda);
+
+        myThread2.run();
+    }
+}
