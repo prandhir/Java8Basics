873df8e299b797d83d713bb5a17b52e77e103522
Pankaj <pankaj.randhir@gmail.com>
2018-12-20 15:49:01 -0500

    extended greet method

---
+++ src/io/javabrain/TypeInferenceExample.java
@@ -0,0 +1,21 @@
+package io.javabrain;
+
+public class TypeInferenceExample {
+
+
+    public static void printLambda(lambdaInterface l){
+        System.out.println(l.getLength("this is string"));
+    }
+
+
+    public static void main(String[] args) {
+
+        printLambda(s -> s.length());
+    }
+
+
+    interface lambdaInterface {
+        int getLength(String s);
+    }
+
+}
